42c580fef147be3553fc57e715ba413d15594635
Adam LeFevre <adam@lefevre.app>
2022-01-01 09:03:57 -0800

    moved convert logic

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -4,9 +4,8 @@ import app from '../index';
 const request = supertest(app);
 
 describe('Test homepage functionality', () => {
-  it('Resolves homepage', async (done) => {
+  it('Resolves homepage', async () => {
     const response = await request.get('/');
     expect(response.status).toBe(200);
-    done();
   });
 });
